6558305b35be7d39e4b2be553fce1b20a9f9fffa
Miray Durgun <miraydurgun67@gmail.com>
2023-09-12 21:58:20 +0300

    Add project files.

---
+++ Kutuphane.Repository/Shared/Abstract/IRepository.cs
@@ -0,0 +1,34 @@
+﻿using Kutuphane.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kutuphane.Repository.Shared.Abstract
+{
+    public interface IRepository<T> where T : BaseModel
+    {
+        //varsaylım ki T - Yazar
+        IEnumerable<T> GetAll();
+        void Add(T item);
+        void AddRange(IEnumerable<T> items);
+
+        void Update(T item);
+        void Remove(T item);
+
+        void RemoveRange(IEnumerable<T> items);
+
+        T GetById(int id);
+
+        T GetFirstOrDefault(Expression<Func<T, bool>> filter);
+
+        void Save();
+
+
+        
+
+
+    }
+}
---
+++ Kutuphane/Controllers/YazarController.cs
@@ -0,0 +1,82 @@
+﻿using Kutuphane.Data;
+using Kutuphane.Models;
+using Kutuphane.Repository.Abstract;
+using Kutuphane.Repository.Shared.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kutuphane.Controllers
+{
+    public class YazarController : Controller
+    {
+        //Dependency Injection
+        private readonly IYazarRepository _repo;
+        
+
+        public YazarController(IYazarRepository repo)
+        {
+            _repo = repo;
+          
+        }
+
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+
+        //CONCRETE
+        public IActionResult GetAll()
+        {
+
+            
+            return Json(new { data = _repo.GetAll() });
+        }
+               
+        public IActionResult Delete(int id)
+        {
+            // Yazar yazar = _context.Yazarlar.Where(y => y.Id == id).First();
+
+            // Yazar yazar = _context.Yazarlar.FirstOrDefault(x => x.Id == id);
+
+            _repo.Remove(_repo.GetById(id));
+            _repo.Save();
+           
+
+            
+
+
+            return RedirectToAction("Index");
+
+        }
+             
+
+        public IActionResult Upsert(int id)
+        {
+            if (id != 0)
+            {
+                return View(_repo.GetById(id));
+            }
+            else {
+                return View();
+            }
+          
+        }
+        [HttpPost]
+        public IActionResult Upsert(Yazar yazar)
+        {
+            if(yazar.Id==0)
+            {
+                _repo.Add(yazar);
+                _repo.Save();
+            }
+            else
+            {
+                _repo.Update(yazar);
+                _repo.Save();
+            }
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
